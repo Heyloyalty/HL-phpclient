c58932057851489d06a73c6021a598c70537f3aa
skourene <skou.rene@gmail.com>
2016-07-05 12:17:48 +0200

    added accounts methods

--- src/HL-phpclient/HLAccounts.php
+++ src/HL-phpclient/HLAccounts.php
@@ -10,4 +10,29 @@ class HLAccounts
     {
         $this->setResellerClient($resellerClient);
     }
+
+    public function getAccounts()
+    {
+
+    }
+
+    public function getAccount($id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update($id,$params)
+    {
+
+    }
+
+    public function delete($id)
+    {
+        
+    }
 }
