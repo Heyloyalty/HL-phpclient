12188852e562ca1fff55a79bf77919a33cdb6348
skourene <skou.rene@gmail.com>
2016-07-12 23:16:58 +0200

    first unittest

--- tests/HL-phpclient/HLMembersTest.php
+++ tests/HL-phpclient/HLMembersTest.php
@@ -1,14 +1,18 @@
 <?php namespace Phpclient;
+use Phpclient\HLMembers;
+use Phpclient\HLClient;
 
 class HLMembersTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-
+        $this->client = new HLClient('DTd7K5yfQFgyLcTS','zo8z8pRy0bRKqMxojcq0t1jVjXzE623L');
+        $this->object = new HLMembers($this->client);
     }
 
-    public function testistrue()
+    public function testGetMembers()
     {
-        $this->assertEquals(3,3);
+        $result = $this->object->getMembers(3753);
+        $this->assertArrayHasKey('response',$result);
     }
 }
