eba42ac982ee38f5f2e8b5ef316c7a24b53ee14f
skourene <skou.rene@gmail.com>
2016-07-12 22:58:29 +0200

    setup unittest

---
+++ tests/HL-phpclient/HLMembersTest.php
@@ -0,0 +1,14 @@
+<?php namespace Phpclient;
+
+class HLMembersTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+
+    }
+
+    public function testistrue()
+    {
+        $this->assertEquals(3,3);
+    }
+}
